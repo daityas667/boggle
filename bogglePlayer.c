4d3eca2ac71739af0c7cefc1beb44664c4bfeb24
daityas667 <57953914+daityas667@users.noreply.github.com>
2019-12-04 20:11:37 -0500

    Add files via upload

--- bogglePlayer.c
+++ bogglePlayer.c
@@ -1,11 +1,11 @@
 /*
 
-  Authors (group members): Brendan Shupp, Christopher Tinkler, John Dewey
-  Email addresses of group members: bshupp2018@my.fit.edu, ctinkler2018@my.fit.edu, jdewey2018@my.fit.edu
-  Group name: Hairy Hooligans
+  Authors (group members):
+  Email addresses of group members:
+  Group name:
 
-  Course: CSE 2010
-  Section: 03
+  Course:
+  Section:
 
   Description of the overall algorithm and main data structures:
 
@@ -19,142 +19,201 @@
 #include <ctype.h>
 #include "bogglePlayer.h"
 #include "word.h"
+#include "hash.h"
 
-char *table[128710]; //Hash Table
+//Struct to hold a letter's position
+typedef struct pos{
 
-void printTable(void){
-    for(int i = 0; i < 128710; i++){
-        if(table[i] != NULL)
-            printf("%d: %s\n", i, table[i]);
-    }
+	//Coordinates on map array
+	int y, x;
+
+}map_pos;
+
+//Struct to hold letter and its position
+typedef struct letter{
+
+	char letter;
+	map_pos position;
+
+}letter;
+
+//Library struct, holds values used in getWord()
+typedef struct library{
+
+	map_pos neighbor1, neighbor2;
+	char letters[16];
+	letter word[17];
+	short remaining;
+	short letter_pos;
+
+}library_file;
+
+//Function that returns 1 if two nodes are neighbors, 0 if they are not
+short isNeighbor(library_file* library){
+
+	short neighbor_check = 0;
+
+	//Check that x/y coords of node 1 are within 1 of the x/y coords of node 2
+	if(library->neighbor1.y == (library->neighbor2.y)+1 || library->neighbor1.y == (library->neighbor2.y)-1 || library->neighbor1.y == library->neighbor2.y){
+		if(library->neighbor1.x == (library->neighbor2.x)+1 || library->neighbor1.x == (library->neighbor2.x)-1 || library->neighbor1.x == library->neighbor2.x){
+			neighbor_check = 1;
+		}
+	}
+
+return neighbor_check;
 }
 
-int getHash(char *word, int length){
-    int value = 0;
-    for(int i = 0; i < length; i++)
-        value += (int)word[i];
-    
-    value *= 2027;
-    while(value > 128710)
-        value /= 47;
-    
-    return value;
-}
+//Function that generates all possible words and checks against dictionary
+void getWord(WordList* myWords, library_file* library,char* word){
 
-void insert(char *word){
-    int index = getHash(word, strlen(word)), spotVisited = 1;
-    if(table[index] != NULL){
-        for(int i = 0; table[index] != NULL; i++){
-            if(spotVisited == 128710)
-                return;
-            index += i;
-            if(index > 128710)
-                index %= 128710;
-            spotVisited++;
-        }
-    }
-    table[index] = (char*)malloc(30 * sizeof(char));
-    strcpy(table[index], word);
-}
+	//Check if word is in dictionary
+	short step = 0;
+	while(library->word[step].letter!='\0'){
+    word[step]=library->word[step].letter;
+		step++;
+	}
 
-int checkWord(char *word){
-    int index = getHash(word, strlen(word));
-    if(table[index] == NULL)
-        return 0;
-    
-    if(strcmp(word, table[index])){
-        for(int i = 1; strcmp(table[index], word) != 0; i++){
-            index += 1;
-            if(index > 128710)
-                index %= 128710;
-            if(table[index] == NULL)
-                return 0;
-        }
+  //Format word string to remove excess characters
+  for(short i=step;i<17;i++){
+    word[i]='\0';
+  }
+
+  //Check whether word will be added to list
+  short duplicate = 0;
+  short word_replaced = 0;
+  short index = myWords->length;
+  if(checkWord(word)){
+    for(short i=0;i<myWords->length;i++){
+      if(!strcmp(myWords->wordlist[i].word,word))//Check that word isn't in list
+        duplicate = 1;
+      if(strlen(myWords->wordlist[i].word)<strlen(word)){//Check if word will score more points than another word on the list
+        index = i;
+        word_replaced = 1;
+      }
     }
-    if(strcmp(table[index], word))
-        return 1;
-    else
-        return 0;
+
+    //Add word to list if it has open positions or a shorter word
+    if(duplicate==0&&myWords->length<20||word_replaced==1&&duplicate==0){
+      if(word_replaced==0)//Don't update length if word was replaced
+        setWordListLength(myWords,myWords->length+1);
+      setWord(&myWords->wordlist[index],word);
+      setPathLength(&myWords->wordlist[index],step);
+      for(short i=0;i<step;i++){//Add path
+        setLetterRowAndCol(&myWords->wordlist[index],i,library->word[i].position.y,library->word[i].position.x);
+      }
+
+    }
+  }
+
+	//Set first letter of word
+	if(library->remaining==16){
+		library->remaining--;
+		for(short i=0;i<16;i++){
+      //Define first letter node of word
+			library->word[library->letter_pos].letter = library->letters[i];
+			library->word[library->letter_pos].position.y = i/4;
+			library->word[library->letter_pos].position.x = i%4;
+
+      //Remove used char from chars available from use
+			library->letters[i]='#';
+
+      //Iterate right in word
+			library->letter_pos++;
+
+      //Recursive call with updated array of available characters
+			getWord(myWords,library,word);
+
+      //Reset values
+			for(short j=library->letter_pos;j<17;j++){
+				library->word[j].letter = '\0';
+			}
+			library->letter_pos--;
+			library->letters[i] = library->word[library->letter_pos].letter;
+		}
+  //Return from getWord when all characters have been used as the first character in the word
+	return;
+	}
+
+	//Add letters to word after first characted is set
+	if(library->remaining>11){//Limit words to size 5
+    //Use all remaining valid characters
+    for(short i=0;i<16;i++){
+			if(library->letters[i]!='#'){
+
+        //Set values of board positions to be checked
+				library->neighbor2.y = i/4;
+				library->neighbor2.x = i%4;
+				library->neighbor1.y = library->word[(library->letter_pos)-1].position.y;
+				library->neighbor1.x = library->word[(library->letter_pos)-1].position.x;
+
+				//If character i in letter array is a valid letter (neighbor) to follow previous, add to word and update library
+				if(isNeighbor(library)==1){
+
+          //Add character to word array with its position
+					library->word[library->letter_pos].letter = library->letters[i];
+					library->word[library->letter_pos].position.y = i/4;
+					library->word[library->letter_pos].position.x = i%4;
+
+          //Remove character from available char array
+					library->letters[i] = '#';
+
+          //Iterate right in word
+					library->letter_pos++;
+
+          //Update # of available chars
+					library->remaining--;
+
+          //Recursive call with updated array of available characters
+					getWord(myWords,library,word);
+
+          //Reset values
+					library->remaining++;
+					for(short j=library->letter_pos;j<17;j++){
+						library->word[j].letter = '\0';
+					}
+					library->letter_pos--;
+					library->letters[i] = library->word[library->letter_pos].letter;
+				}
+			}
+		}
+	}
+
+//Return to recursive calls
+return;
 }
 
 // initialize BogglePlayer with a file of English words
 void initBogglePlayer(char* word_file) {
-    int numWords = 0;
-    char str[31];
-    FILE *inp = fopen(word_file, "r");
-    while(!feof(inp)){
-        fgets(str, 30, inp);
-        int size = strlen(str) - 2;
-        str[size] = '\0';
-        for(int i = 0; i < size; i++){
-            str[i] = toupper(str[i]);
-        }
-        if(size > 3 && size < 9){
-            numWords++;
-            insert(str);
-        }
-    }/*
-    printTable();
-    printf("Num of Words: %d\n", numWords);
-    char checkStr[30];
-    printf("Enter a word to check: ");
-    scanf("%s", checkStr);
-    if(checkWord(checkStr))
-        printf("Word Found!\n");
-    else
-        printf("Word Not Found!\n");*/
-}
 
+  buildDictionary(word_file);
 
-
-void      sampleWordList(WordList* myWords);   // a sample function to populate a word list
-
-// based on the board, find valid words
-//
-// board: 4x4 board, each element is a letter, 'Q' represents "QU",
-//    first dimension is row, second dimension is column
-//    ie, board[row][col]
-//
-// Return at most 20 valid words in UPPERCASE and
-//    their paths of locations on the board in myWords
-//
-// See word.h for details of the struct for Word, WordList, Location, and access functions
-/*
-WordList* getWords(char board[4][4]) {
-
-    WordList* myWords = getNewWordList(); 
-
-    sampleWordList(myWords);
-    
-    return myWords;
-}*/
-
-void acquireWord(libary_file* library){
-    short step = 0;
-    while(library->word[step].letter != '\0')
-        step++;
 }
 
 WordList* getWords(char board[4][4]) {
 
-    WordList* myWords = getNewWordList(); 
+    WordList* myWords = getNewWordList();
+    myWords->length = 0;
 
-    sampleWordList(myWords);
-    
+    //Declare library and initialize values
+    library_file library;
+    for(short y=0;y<4;y++){
+      for(short x=0;x<4;x++){
+        library.letters[(y*4)+x]=board[y][x];
+      }
+    }
+    for(short i=0;i<17;i++){
+  		library.word[i].letter = '\0';
+  	}
+  	library.letter_pos = 0;
+  	library.remaining = 16;
+  	char word[17];
+    for(int i=0;i<17;i++){
+      word[i]='\0';
+    }
+
+    //Get words from boggle board and add them to word list
+    getWord(myWords,&library,word);
+
+    //Return words found
     return myWords;
-}
-
-
-
-// a sample list of one word
-void sampleWordList(WordList* myWords)  {
-    setWordListLength(myWords, 1);      // number of words on the list 
-    Word *word = getWordFromWordList(myWords, 0);   // first word
-
-    setWord(word, "cat");               // word string
-
-    setPathLength(word, 3);             // word path
-    setLetterRowAndCol(word, 0, 1, 2);  // letter 0 is at row 1 and column 2
-    setLetterRowAndCol(word, 1, 1, 3);
-    setLetterRowAndCol(word, 2, 2, 3);
 }
--- hash.h
+++ hash.h
@@ -8,81 +8,98 @@
 #include <ctype.h>
 #include <time.h>
 
-char *table[128710]; //hash table
+char *table[350000]; //hash table - size is set to 350000 to make the load factor .67
 
 /* Table Functions */
 
-void printTable(void){
-	for (int i = 0; i < 128710; i++){
-		if (table[i] != NULL){
-			printf("%d: %s\n", i, table[i]);
-		}
-	}
-}
-
+/*
+	getHash gets a hash value for a given word
+	Input Parameters:
+		char *word - word to hash
+		short length - length of word
+	Return Value:
+		int value - hash of word
+*/
 int getHash(char *word, short length){
 	int value = 0;
-	for (short i = 0; i < length; i++){
+	for (short i = 0; i < length; i++){ //store the sum of each all the string's characters in value
 		value += (int)word[i];
 	}
-	value *= 2027;
-	while (value > 128710){
+	value *= 2027; //compression by multiplying by a really big prime number then dividing by a smaller prime until in range
+	while (value > 350000){
 		value /= 47;
 	}
 	return value;
 }
 
+/*
+	insert inserts a word into the hash table using linear probing
+	Input Parameters:
+		char *word - word to insert
+*/
 void insert(char *word){
-	int index = getHash(word, strlen(word)), spotsVisited = 1;
-	if (table[index] != NULL){
+	int index = getHash(word, strlen(word)), spotsVisited = 1; //get initial hash
+	if (table[index] != NULL){ //if there is already a word in the original hash's place, do a linear prove until an empty space is found
 		for (short i = 1; table[index] != NULL; i++){
-			if (spotsVisited == 128710){
+			if (spotsVisited == 350000){
 				return;
 			}
 			index += i;
-			if (index > 128710){
-				index %= 128710;
+			if (index > 350000){
+				index %= 350000;
 			}
 			spotsVisited++;
 		}
 	}
-	table[index] = (char *)malloc(30 * sizeof(char));
+	table[index] = (char *)malloc(30 * sizeof(char)); //make space for word in table then add word
 	strcpy(table[index], word);
 }
 
+/*
+	buildDictionary adds each word in the file of words into the hash table
+	Input Parameters:
+		char *fileName - name of file containing words
+*/
 void buildDictionary(char *fileName){
 	char str[31];
-	FILE *inp = fopen("words.txt", "r");
-	while (!feof(inp)){
+	FILE *inp = fopen(fileName, "r");
+	while (!feof(inp)){ //while there are words left to add, scan in the strings and store them
 		fgets(str, 30, inp);
 		short size = strlen(str) - 2;
-		for (short i = 0; i < size; i++){
+		for (short i = 0; i < size; i++){ //make strings all uppercase
 			str[i] = toupper(str[i]);
 		}
 		str[size] = '\0';
-		if (size > 3 && size < 9){
+		if (size > 3 && size < 6){ //insert only if string is in a certain size range reduce unnecessary processes
 			insert(str);
 		}
 	}
 }
 
+/*
+	checkWord checks if a word is in the hash table or not
+	Input Parameters:
+		char *word - word to check
+	Return Value:
+		true or false value - 1 for true, 0 for false
+*/
 int checkWord(char *word){
-	int index = getHash(word, strlen(word));
-	if (table[index] == NULL){
+	int index = getHash(word, strlen(word)); //get word's hash
+	if (table[index] == NULL){ //if spot is empty, the word is not in the list
 		return 0;
 	}
-	if (strcmp(word, table[index])){
+	if (strcmp(word, table[index])){ //if the word in the space of the hash, then do a linear probe to find the word
 		for (short i = 1; strcmp(table[index], word) != 0; i++){
 			index += i;
-			if (index > 128710){
-				index %= 128710;
+			if (index > 350000){
+				index %= 350000;
 			}
-			if (table[index] == NULL){
+			if (table[index] == NULL){ //if an empty space if encountered the word is not in the list
 				return 0;
 			}
 		}
 	}
-	if (!strcmp(table[index], word)){
+	if (!strcmp(table[index], word)){ //return true if word was found
 		return 1;
 	} else {
 		return 0;
